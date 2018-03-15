8661858ce1ec3009c70c4e48f4db1a325a64e278
Brock Tubre <brock.tubre@csra.com>
2018-03-14 19:40:29 -0500

    Moved somethings around

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { BugsService } from './bugs/bugs.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,24 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'app';
-
-  constructor(private bugsService: BugsService) {
-
-  }
-
-  ngOnInit() {
-    console.log('AppComponent ngOnInit');
-    // this.getBugs();
-  }
-
-  getBugs() {
-    this.bugsService.getAllBugs().subscribe(
-      data => {
-        console.log(data);
-      },
-      error => console.error(error)
-    );
-  }
 }
--- src/app/bugs/bugs.component.ts
+++ src/app/bugs/bugs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BugsService } from './bugs.service';
 
 @Component({
   selector: 'app-bugs',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BugsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bugsService: BugsService) {
+
+  }
 
   ngOnInit() {
     console.log('BugsComponent ngOnInit');
+    // this.getBugs();
+  }
+
+  getBugs() {
+    this.bugsService.getAllBugs().subscribe(
+      data => {
+        console.log(data);
+      },
+      error => console.error(error)
+    );
   }
 
 }
