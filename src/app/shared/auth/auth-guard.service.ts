d8bf185a6f71aae5093766eb5341a64a397d018d
Brock Tubre <brock.tubre@csra.com>
2018-03-21 23:16:00 -0500

    Refresh working

--- src/app/shared/auth/auth-guard.service.ts
+++ src/app/shared/auth/auth-guard.service.ts
@@ -1,19 +1,37 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
 import { RoleService } from '../role/role.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-  constructor(public auth: AuthService, public router: Router, public roleService: RoleService) {}
+  constructor(private auth: AuthService,
+              private router: Router,
+              private roleService: RoleService,
+              private angularFireAuth: AngularFireAuth) {}
 
-  canActivate(): boolean {
-    if (!this.auth.isLoggedIn()) {
-      this.router.navigate(['home']);
-      return false;
-    }
-    return true;
-  }
+  // canActivate(): boolean {
+  //   debugger;
+  //   if (!this.auth.isLoggedIn()) {
+  //     this.router.navigate(['home']);
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
+  canActivate():Observable<boolean>|boolean {
+    return this.angularFireAuth.authState.map((auth) => {
+        if (auth) {
+            console.log('authenticated');
+            return true;
+        }
+        console.log('not authenticated');
+        this.router.navigateByUrl('login');
+        return false;
+    }).first();
+}
 }
